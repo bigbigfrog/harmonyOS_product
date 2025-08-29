0a3b9ff14f2a9e97a9ec1bbc90c6ad51cb355794
voxelkana <voxelkana@gmail.com>
2025-08-29 16:56:01 +0800

    router路由更改

--- config/routes.ts
+++ config/routes.ts
@@ -1,4 +1,6 @@
-﻿export default [
+﻿import { layout } from "@/app";
+
+export default [
   {
     path: '/user',
     layout: false,
@@ -12,11 +14,27 @@
             path: '/user/register',
             component: './user/register',
           },
+
         ],
       },
     ],
   },
+
+  // { path: '/dashboard/main', name: '仪表盘', icon: 'smile', component: './dashboard/Main' ,layout: false},
+  {
+    path: '/dashboard',
+    name: '控制面板',
+    icon: 'dashboard', layout: false,
+
+    routes: [
+      { path: '/dashboard/main', name: '总览', component: './dashboard/Main',layout: false},
+      { path: '/dashboard/livingroom', name: '客厅', component: './dashboard/livingroom',layout: false},
+      { path: '/dashboard/bedroom', name: '厨房', component: './dashboard/bedroom',layout: false},
+    ],
+  },
+
   { path: '/welcome', name: '欢迎', icon: 'smile', component: './Welcome' },
+
   {
     path: '/admin',
     name: '管理页',
